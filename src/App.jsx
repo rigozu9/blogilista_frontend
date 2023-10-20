43a3be921fb928afb9b48ff89a3e01569d474a70
Riku <134082872+rigozu9@users.noreply.github.com>
2023-10-20 12:19:43 +0300

    5.10 done

--- src/App.jsx
+++ src/App.jsx
@@ -134,9 +134,13 @@ const App = () => {
           createBlog={addBlog}
         />
       </Togglable>
-      {blogs.map(blog => (
-        <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog 
+            key={blog.id}
+            blog={blog}
+            updateLikes={updateLikes}/>)}
     </>
   )
 
