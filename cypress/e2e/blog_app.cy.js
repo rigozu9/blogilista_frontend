cef6ab1d8ce5ee27115a201419f5d21af551fb03
Riku <134082872+rigozu9@users.noreply.github.com>
2023-10-24 13:03:44 +0300

    5.21 ja 5.22 done

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -6,7 +6,13 @@ describe('Blog app', function() {
       username: 'mluukkai',
       password: 'salainen'
     }
+    const testuser = {
+      name: 'Test Man',
+      username: 'testman',
+      password: 'test'
+    }
     cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.request('POST', 'http://localhost:3001/api/users/', testuser)
     cy.visit('http://localhost:5173')
   })
 
@@ -50,6 +56,18 @@ describe('Blog app', function() {
       cy.contains('a new blog Blog By Cypress by Cypress added')
       cy.contains('Blog By Cypress by Cypress')
     })
+    it('A blog can be liked', function() {
+      cy.contains('create new blog').click()
+      cy.get('#title').type('Blog By Cypress')
+      cy.get('#author').type('Cypress')
+      cy.get('#url').type('www.cypress.com')
+      cy.get('#create-button').click()
+      cy.contains('create').click()
+      cy.contains('a new blog Blog By Cypress by Cypress added')
+      cy.contains('Blog By Cypress by Cypress')
+      cy.contains('View More').click()
+      cy.contains('like').click()
+    })
   })
   describe('when logged in and new blog created', function() {
     beforeEach(function() {
@@ -66,9 +84,19 @@ describe('Blog app', function() {
       cy.contains('a new blog Blog By Cypress by Cypress added')
       cy.contains('Blog By Cypress by Cypress')
     })
-    it('A blog can be liked', function() {
+    it('A blog can be deleted if logged in acc created it', function() {
       cy.contains('View More').click()
-      cy.contains('like').click()
+      cy.contains('REMOVE').click()
+      cy.contains('blog deleted succesfully!')
+    })
+    it('Remove button not seen if not creator logged in', function() {
+      cy.contains('logout').click()
+      cy.contains('log in').click()
+      cy.get('#username').type('testman')
+      cy.get('#password').type('test')
+      cy.get('#login-button').click()
+      cy.contains('View More').click()
+      cy.contains('REMOVE').should('not.exist')
     })
   })
 })
