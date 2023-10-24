a177023e37eff04cd52d566b24d5ed94992a49dc
Riku <134082872+rigozu9@users.noreply.github.com>
2023-10-24 12:20:18 +0300

    5.18 done

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,10 +1,36 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:5173')
   })
 
   it('Login form is shown', function() {
     cy.contains('log in to application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('wronginput')
+      cy.get('#password').type('wronginput')
+      cy.get('#login-button').click()
+
+      cy.contains('invalid username or password')
+    })
   })
 })
